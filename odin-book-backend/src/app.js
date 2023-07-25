cf85cb1feda9e4d6b2af98fa1e4238f1e59ddb85
amc131 <76466256+amc131@users.noreply.github.com>
2023-07-24 21:56:37 -0700

    Format files into src directory and use router

--- odin-book-backend/src/app.js
+++ odin-book-backend/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const router = require('./routes/indexRouter');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/index', router);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
