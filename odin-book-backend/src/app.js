c8ec8f65a7b547995ce602f7c0840f5a06e688db
amc131 <76466256+amc131@users.noreply.github.com>
2023-07-25 18:36:34 -0700

    Configure babel and nodemon

--- odin-book-backend/src/app.js
+++ odin-book-backend/src/app.js
@@ -3,17 +3,10 @@ const router = require('./routes/indexRouter');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/index', router);
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+module.exports = app;
